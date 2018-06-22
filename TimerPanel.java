a97c41101c0111d0527e11b67986df5f9b588f43
Leerng <39564803+Leerng@users.noreply.github.com>
2018-06-22 09:32:34 -0700

    Create TimerPanel.java

---
+++ TimerPanel.java
@@ -0,0 +1,17 @@
+public class TimerPanel extends JPanel {
+    private static final long serialVersionUID = 1L;
+    
+    private int width = 150;
+    private int height = 24;
+    private String timeString = "00:00:00";
+    
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        g.drawString(0, height);
+    }
+    
+    public Dimension getPreferredSize() {
+        Dimension size = new Dimension(width, height);
+        return size;
+    }
