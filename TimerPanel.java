74ed4ac23d70c70654d26d7b22487cb9bd40312d
Leerng <39564803+Leerng@users.noreply.github.com>
2018-07-03 19:56:49 -0700

    Update TimerPanel.java

--- TimerPanel.java
+++ TimerPanel.java
@@ -1,17 +1,84 @@
-public class TimerPanel extends JPanel {
-    private static final long serialVersionUID = 1L;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class TimerPanel extends JPanel implements Runnable {
+	
+	private static final long serialVersionUID = 1L;
     
     private int width = 150;
     private int height = 24;
     private String timeString = "00:00:00";
+    private long time = 10;
+    private Thread timerThread;
+    
+    public TimerPanel(long time, Font font) {
+    	setTime(time);
+    	setFont(font);
+    	height = font.getSize();
+    	FontMetrics fm = getFontMetrics(font);
+    	width = fm.stringWidth(timeString); 
+    }
     
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.BLACK);
-        g.drawString(0, height);
+        g.drawString(timeString, 0, height);
     }
     
     public Dimension getPreferredSize() {
         Dimension size = new Dimension(width, height);
         return size;
     }
+    
+    public void setTime(long time) {
+    	this.time = time;
+    	long h = time / 3600;
+    	long m = (time / 60) % 60;
+    	long s = time % 60;
+    	timeString = String.format("%02d:%02d:%02d", h, m, s);
+    	repaint();
+    }
+    
+    public void start() {
+    	stop();
+    	timerThread = new Thread(this);
+    	timerThread.start();
+    }
+    
+    public void stop() {
+    	if (timerThread != null) {
+    		timerThread.interrupt();
+    		timerThread = null;
+    	}
+    }
+    
+    public void run() {
+    	while (time > 0) {
+    		time -= 1;
+    		setTime(time);
+    		try {
+    			Thread.sleep(1000);
+    		}
+    		catch (InterruptedException e) {
+    			return;
+    		}
+    	}
+    	timesUp();
+    }
+    
+    protected void timesUp() {
+    	String message = "Time's Up!";
+    	JOptionPane.showMessageDialog(this, message);
+    }
+    
+    public long getTime() {
+    	return time;
+    }
+
+}
